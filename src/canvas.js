a68692c56fb467afb8c91ffd760c5263b2abab5f
TotallySheen <47955002+TotallySheen@users.noreply.github.com>
2020-10-28 22:59:44 -0400

    small noise update

--- src/canvas.js
+++ src/canvas.js
@@ -176,7 +176,7 @@ function draw(params={},progress=0){
 			// data[i+2] is the blue channel
 			// data[i+3] is the alpha channel
 			data[i] = data[i+1] = data[i+2] = 0; // zero out the red and green and blue channels
-            data[i+2] = 255; // make the red channel 100% red
+            data[i+1] = 255; // make the red channel 100% red
         } // end if
         if (params.colorFilter == "invert"){
             let red = data[i], green = data[i+1], blue = data[i+2];
